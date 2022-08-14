f7faa0d244936e18df30d6c594a31eabc4047ba5
18BLC1119 <ashish.kumar2018b@vitstudent.ac.in>
2022-08-14 22:03:28 +0530

    added some string methods from ruby

---
+++ section6.rb
@@ -0,0 +1,14 @@
+#Substitution
+x="tableta"
+y=x.sub("ta","ra")
+p y+" "+x
+y=x.gsub("ta","ra")
+p y+" "+x
+p "malayalam".reverse
+p "Ashish".reverse
+z= "AsHish"
+p z.downcase
+p z.upcase
+p "Ashish".include?("sh")
+p "Ashish".include?("as")
+p "Ashish".include?("As")
