b6091b11ac31b1bbb10dd5f03abe9a5bc7c47a42
18BLC1119 <ashish.kumar2018b@vitstudent.ac.in>
2022-08-15 12:51:27 +0530

    added section7

---
+++ section7.rb
@@ -0,0 +1,24 @@
+p "Hello, world!"
+def funDec
+  p "Hello, World! from funDec1"
+end
+funDec()
+def funName
+  puts "Hi! I am Ashish."
+end
+funName()
+def funMul(name,x)
+  p name
+  p x
+end
+funMul("Ashish",18)
+funMul "Ashish",19
+
+def funk(name)
+  name
+end
+p funk("ashish")
+def funkv2(name)
+  return name
+end
+p funk("Aashish")
